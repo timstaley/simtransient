cb90a3df0d046f8cc06504c3bedb75ad971c1848
Tim Staley <tim.staley@astro.ox.ac.uk>
2015-03-16 12:51:51 +0000

    Add some unit tests.

---
+++ simtransient/tests/test_supernova.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from unittest import TestCase
+import numpy as np
+from scipy.stats import multivariate_normal
+from simtransient.models.supernova import Sn1aOpticalEnsemble
+
+
+class TestSN1a(TestCase):
+    def setUp(self):
+        ens = self.ens = Sn1aOpticalEnsemble()
+        self.scipy_rv = multivariate_normal(mean=ens.gpars.loc['mu'],
+                                            cov=ens.gpar_cov)
+
+    def test_prior(self):
+        test_samples = self.scipy_rv.rvs(int(1e5))
+        scipy_logpdfs = self.scipy_rv.logpdf(test_samples)
+        lnpriors = self.ens.lnprior(test_samples)
+        self.assertTrue(np.allclose(scipy_logpdfs,
+                                    lnpriors))
---
+++ simtransient/tests/test_utils.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+from unittest import TestCase
+import numpy as np
+from scipy.stats import multivariate_normal
+from scipy.spatial.distance import mahalanobis
+from simtransient import utils
+
+
+class TestMahalanobis(TestCase):
+    def setUp(self):
+        #A quick-and-dirty ndim covariance, courtesy of the emcee docs.
+        ndim = 10
+        self.means = np.random.rand(ndim)
+        cov = 0.5 - np.random.rand(ndim ** 2).reshape((ndim, ndim))
+        cov = np.triu(cov)
+        cov += cov.T - np.diag(cov.diagonal())
+        self.cov = np.dot(cov,cov)
+        self.icov = np.linalg.inv(self.cov)
+
+        self.rv = multivariate_normal(mean = self.means,
+                                      cov=self.cov)
+        self.n_samples = int(1e4)
+        self.sample = self.rv.rvs(self.n_samples)
+
+    def test_scipy_equality(self):
+        diff = self.sample - self.means
+        simtransient_results = utils.mahalanobis_sq(self.icov, diff)
+        scipy_results = np.zeros_like(simtransient_results)
+        for idx, s in enumerate(self.sample):
+            scipy_results[idx] = mahalanobis(self.means, s, self.icov)**2
+
+        self.assertTrue(np.allclose(scipy_results, simtransient_results))
